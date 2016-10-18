bb68a37dafb37c39e6ed18becc5d4daa5cb08ba1
SHADI JAM <shadijam@SHADIs-MacBook-Pro.local>
2016-10-18 14:29:07 -0500

    Added nuget

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
 
 namespace ConsoleApplication
 {
@@ -6,7 +9,20 @@ namespace ConsoleApplication
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            prompt().Wait();
+            // Console.WriteLine(result);
+        }
+
+        public static async Task prompt(){
+            string input = Console.ReadLine();
+            string result = await getUrl("http://google.com"+input);
+            Console.WriteLine(result);
+        }
+
+        public static async Task<string> getUrl(string url){
+            var http = new HttpClient();
+            string reply = await http.GetStringAsync(url);
+            return reply;
         }
     }
 }
