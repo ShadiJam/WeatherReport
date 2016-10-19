b41dd270335fc6aaf12b60a3c50e70dee6c5b8f5
SHADI JAM <shadijam@SHADIs-MacBook-Pro.local>
2016-10-19 14:20:11 -0500

    add model and api files

---
+++ Model.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace Weather2 {
+// dark sky output
+    public class RootObjectGoogle
+{
+    public double latitude { get; set; }
+    public double longitude { get; set; }
+    public string timezone { get; set; }
+    public int offset { get; set; }
+    public Currently currently { get; set; }
+    public Minutely minutely { get; set; }
+    public Hourly hourly { get; set; }
+    public Daily daily { get; set; }
+}
+public class Minutely
+{
+    public string summary { get; set; }
+    public string icon { get; set; }
+     public int time { get; set; }
+    public int precipIntensity { get; set; }
+    public int precipProbability { get; set; }
+}
+public class Currently : Minutely
+{
+    public double temperature { get; set; }
+    public double apparentTemperature { get; set; }
+    public double dewPoint { get; set; }
+    public double humidity { get; set; }
+    public double windSpeed { get; set; }
+    public int windBearing { get; set; }
+    public double visibility { get; set; }
+    public double cloudCover { get; set; }
+}
+
+public class Hourly : Minutely
+{
+    public double temperature { get; set; }
+    public double apparentTemperature { get; set; }
+    public double dewPoint { get; set; }
+    public double humidity { get; set; }
+    public double windSpeed { get; set; }
+    public int windBearing { get; set; }
+    public double visibility { get; set; }
+    public double cloudCover { get; set; }
+}
+public class Daily : Hourly
+{
+    public int sunriseTime { get; set; }
+    public int sunsetTime { get; set; }
+    public double moonPhase { get; set; }
+    public double temperatureMin { get; set; }
+    public double temperatureMax { get; set; }
+}
+    
+// geo locator output 
+
+public class RootObject
+{
+        public Result results { get; set; }
+}
+public class Result
+{
+    public Geometry geometry { get; set; }
+}
+public class Geometry
+{
+    public Location location { get; set; }
+}
+
+public class Location
+{
+    public double lat { get; set; }
+    public double lng { get; set; }
+}
+
+}
--- Program.cs
+++ Program.cs
@@ -1,20 +1,33 @@
 ﻿using System;
-using System.Net.Http;
+using System.Collections.Generic;
+using System.Linq;
 using Newtonsoft.Json;
 using System.Threading.Tasks;
-using System.Net;
+using System.Net.Http;
 
-namespace ConsoleApplication
-{
-    public class Program
-    {
+namespace Weather2 {
+    class Program {
+        public static void Main(){
+            prompt().Wait();
+        }
+
+        public static async Task prompt(){
+            Console.WriteLine("Welcome to the Weather Report! Enter your city and state or your zip code:");
+            string addInput = Console.ReadLine();
+            var geoKey = "AIzaSyDeOZwDv9PR7Wk5aL9l8KxUlTIDZzXmC4w";
+            API googleUrl = new API($"https://maps.googleapis.com/maps/api/geocode/json?address={addInput}&key={geoKey}");
+            GoogleAPIModel data = await googleUrl.GetData<GoogleAPIModel>();
+            Console.WriteLine(data.results.Count());
+            Console.WriteLine(JsonConvert.SerializeObject(data));
+        }
+    }
+}
+
+/*
         public static void Main(string[] args)
         {
             prompt().Wait();
-            Console.WriteLine("Welcome to the Weather Report! Please enter your city and state, or your zip code to see the weather forecast!");
-            string addInput = Console.ReadLine();
-            Console.ReadLine();
-            var geoKey = "AIzaSyDeOZwDv9PR7Wk5aL9l8KxUlTIDZzXmC4w";
+   
             
             var googleUrl = String.Format("https://maps.googleapis.com/maps/api/geocode/json?address={0}&key={1}", addInput, geoKey);
             var newUser = new WebClient();
@@ -25,7 +38,6 @@ namespace ConsoleApplication
             var darkSkyKey = "589b4821461ba16c289f8b67fe662445";
             var darkSkyUrl = String.Format("https://api.darksky.net/forecast/{0}/{1}", darkSkyKey, latLng);
             
-
         }
 
         public static async Task prompt(){
@@ -41,9 +53,9 @@ namespace ConsoleApplication
             string reply = await http.GetStringAsync(url);
             return reply;
         // ask client for address/zip/city and state - DONE
-        // take input and send that to geo locator which locates lng and lat
-        // take return from geolocator and input into darksky
-        // return forecast from darksky that includes the following:
+        // take input and send that to geo locator which locates lng and lat - DONE?
+        // take return from geolocator and input into darksky - DONE?
+        // return forecast from darksky that includes the following: 
         //      currently, 
         //      daily for next 8 days, 
         //      sunrise and sunsetTime
@@ -51,119 +63,6 @@ namespace ConsoleApplication
 
         
         }
-// dark sky output
-    public class RootObject
-{
-    public double latitude { get; set; }
-    public double longitude { get; set; }
-    public string timezone { get; set; }
-    public int offset { get; set; }
-    public Currently currently { get; set; }
-    public Minutely minutely { get; set; }
-    public Hourly hourly { get; set; }
-    public Daily daily { get; set; }
- 
-}
-public class Minutely
-{
-    public string summary { get; set; }
-    public string icon { get; set; }
-     public int time { get; set; }
-    public int precipIntensity { get; set; }
-    public int precipProbability { get; set; }
-
-
-}
-public class Currently : Minutely
-{
-   
-    public double temperature { get; set; }
-    public double apparentTemperature { get; set; }
-    public double dewPoint { get; set; }
-    public double humidity { get; set; }
-    public double windSpeed { get; set; }
-    public int windBearing { get; set; }
-    public double visibility { get; set; }
-    public double cloudCover { get; set; }
-  
-}
-
-public class Hourly : Minutely
-{
-    public double temperature { get; set; }
-    public double apparentTemperature { get; set; }
-    public double dewPoint { get; set; }
-    public double humidity { get; set; }
-    public double windSpeed { get; set; }
-    public int windBearing { get; set; }
-    public double visibility { get; set; }
-    public double cloudCover { get; set; }
-
-}
-public class Daily : Hourly
-{
-    public int sunriseTime { get; set; }
-    public int sunsetTime { get; set; }
-    public double moonPhase { get; set; }
-  
-    public double temperatureMin { get; set; }
-    public double temperatureMax { get; set; }
-
-}
     }
 }
-// geo locator output 
-public class AddressComponent
-{
-    public string long_name { get; set; }
-    public string short_name { get; set; }
-    public List<string> types { get; set; }
-}
-
-public class Location
-{
-    public double lat { get; set; }
-    public double lng { get; set; }
-}
-
-public class Northeast
-{
-    public double lat { get; set; }
-    public double lng { get; set; }
-}
-
-public class Southwest
-{
-    public double lat { get; set; }
-    public double lng { get; set; }
-}
-
-public class Viewport
-{
-    public Northeast northeast { get; set; }
-    public Southwest southwest { get; set; }
-}
-
-public class Geometry
-{
-    public Location location { get; set; }
-    public string location_type { get; set; }
-    public Viewport viewport { get; set; }
-}
-
-public class Result
-{
-    public List<AddressComponent> address_components { get; set; }
-    public string formatted_address { get; set; }
-    public Geometry geometry { get; set; }
-    public string place_id { get; set; }
-    public List<string> types { get; set; }
-}
-
-public class RootObject
-{
-    public List<Result> results { get; set; }
-    public string status { get; set; }
-}
-
-
+*/
