ed0327dd27ceb348c6f9db141cf921c0031fc335
SHADI JAM <shadijam@SHADIs-MacBook-Pro.local>
2016-10-20 09:27:28 -0500

    edits to api

--- Program.cs
+++ Program.cs
@@ -14,41 +14,36 @@ namespace Weather2 {
         public static async Task prompt(){
             Console.WriteLine("Welcome to the Weather Report! Enter your city and state or your zip code:");
             string addInput = Console.ReadLine();
-            var geoKey = "AIzaSyDeOZwDv9PR7Wk5aL9l8KxUlTIDZzXmC4w";
-            API googleUrl = new API($"https://maps.googleapis.com/maps/api/geocode/json?address={addInput}&key={geoKey}");
+            API googleUrl = new API($"https://maps.googleapis.com/maps/api/geocode/json?address={addInput}&key=AIzaSyBRBZtk0JyJrAzmp2i9DklBHhjvKwoI0JE");
             darkSkyRO data = await googleUrl.GetData<darkSkyRO>();
             Console.WriteLine(data.results.Count());
             Console.WriteLine(JsonConvert.SerializeObject(data));
 
             //at this point we've pulled the lat and lng from google api and need to send it to dark sky to get the weather
-            data = String.Format("{0},{1}", m.lat, m.lng);
-            
-            var darkSkyUrl = String.Format("https://api.darksky.net/forecast/{0}/{1}", darkSkyKey, latLng);
+         //   var latLng= String.Format("{0},{1}", lat, lng);
+         //   var darkSkyKey = "589b4821461ba16c289f8b67fe662445";
+         //   var darkSkyUrl = String.Format("https://api.darksky.net/forecast/d353c94884828ab143c8633437f899aa/{1},{2}", darkSkyKey, m.lat, m.lng);
         }
     }
 }
 
 /*
-        public static void Main(string[] args)
-        {
-            prompt().Wait();
-   
+        
             
-            var googleUrl = String.Format("https://maps.googleapis.com/maps/api/geocode/json?address={0}&key={1}", addInput, geoKey);
-            var newUser = new WebClient();
             string reply = newUser.DownloadString(googleUrl);
             Location m = JsonConvert.DeserializeObject<Location>(reply);
+            var newUser = new WebClient();
+           
 
-            var latLng= String.Format("{0},{1}", m.lat, m.lng);
-            var darkSkyKey = "589b4821461ba16c289f8b67fe662445";
-            var darkSkyUrl = String.Format("https://api.darksky.net/forecast/{0}/{1}", darkSkyKey, latLng);
+            
+           
             
         }
 
      
         // ask client for address/zip/city and state - DONE
-        // take input and send that to geo locator which locates lng and lat - DONE?
-        // take return from geolocator and input into darksky - DONE?
+        // take input and send that to geo locator which locates lng and lat - DONE
+        // take return from geolocator and input into darksky - 
         // return forecast from darksky that includes the following: 
         //      currently, 
         //      daily for next 8 days, 
